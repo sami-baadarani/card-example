b13ea85686a7d86dbce81e76633b992cd6a70f09
Sami Baadarani <samibaadarani@gmail.com>
2021-04-06 22:18:45 +1000

    feat: change images to provide nicer examples

--- constants/TempData.js
+++ constants/TempData.js
@@ -1,6 +1,6 @@
 const tempData = [
   {
-    avatarSrc: 'https://i.pravatar.cc/',
+    avatarSrc: 'https://i.pravatar.cc/300?img=7',
     name: 'James Smith',
     expertise: 'Clinical psychologist',
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt.`,
@@ -18,43 +18,39 @@ const tempData = [
     }
   },
   {
-    avatarSrc: 'https://i.pravatar.cc/',
+    avatarSrc: 'https://i.pravatar.cc/300?img=1',
     name: 'Mary Johnson',
     expertise: 'Military psychologist',
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt.`,
     availability: {
       nextSession: 'Thursday 8th Apr',
       times: [
-        { time: '8am', extra: '+$25' },
+        { time: '8am' },
         { time: '9am' },
-        { time: '10am' },
+        { time: '10am', extra: '+$25' },
         { time: '11:30am' },
         { time: '12:30am' },
         { time: '1:00pm' },
-        { time: '2:00pm' }
+        { time: '5:00pm', extra: '+$25' }
       ]
     }
   },
   {
-    avatarSrc: 'https://i.pravatar.cc/',
+    avatarSrc: 'https://i.pravatar.cc/300?img=8',
     name: 'John Williams',
     expertise: 'Child psychologist',
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt.`,
     availability: {
       nextSession: 'Thursday 8th Apr',
       times: [
-        { time: '8am', extra: '+$25' },
-        { time: '9am' },
-        { time: '10am' },
-        { time: '11:30am' },
-        { time: '12:30am' },
         { time: '1:00pm' },
-        { time: '2:00pm' }
+        { time: '2:00pm' },
+        { time: '5:00pm', extra: '+$50' }
       ]
     }
   },
   {
-    avatarSrc: 'https://i.pravatar.cc/',
+    avatarSrc: 'https://i.pravatar.cc/300?img=5',
     name: 'Patricia Brown',
     expertise: 'Forensic psychologist',
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt.`,
@@ -72,7 +68,7 @@ const tempData = [
     }
   },
   {
-    avatarSrc: 'https://i.pravatar.cc/',
+    avatarSrc: 'https://i.pravatar.cc/300?img=11',
     name: 'Robert Jones',
     expertise: 'Organizational psychologist',
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt.`,
