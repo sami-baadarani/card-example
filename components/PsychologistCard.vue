6a2270b45c697df64c2d96952658f1ef16c99fc5
Sami Baadarani <samibaadarani@gmail.com>
2021-04-06 19:20:17 +1000

    feat: project init

---
+++ components/PsychologistCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <img src="" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    // name:
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="page__main">
+    <b-container>
+      <b-row>
+        <b-col> </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.page__main {
+}
+</style>
